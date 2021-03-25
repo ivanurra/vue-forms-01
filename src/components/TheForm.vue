9f25615e17b76efd59233a5e7d755821fa26f57b
ivanurra <ivan.urra.dev@gmail.com>
2021-03-25 13:50:56 +0100

    custom control component

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -78,6 +78,9 @@
       </div>
     </div>
     <div class="form-control">
+      <rating-control></rating-control>
+    </div>
+    <div class="form-control">
       <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm"/>
       <label for="confirm-terms">Agree to terms of use?</label>
     </div>
@@ -88,7 +91,11 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue'
 export default {
+  components: {
+    RatingControl
+  },
   data() {
     return {
       userName: '',
